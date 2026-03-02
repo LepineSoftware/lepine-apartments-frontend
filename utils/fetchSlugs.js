fd004db598e87466d112fe9b7ad261ba8fb82d4a
ryanpeterson3 <ryan.peterson@lepinecorp.com>
2026-03-02 11:08:28 -0500

    modified api calls for newsroom posts and slugs

--- utils/fetchSlugs.js
+++ utils/fetchSlugs.js
@@ -1,7 +1,37 @@
-const { appUrl } = require("./appUrl");
+import { apiUrl } from "./apiUrl";
+import qs from "qs";
 
-module.exports = async () => {
-    const slugsUrl = await fetch(`${appUrl}/api/slugs`);
-    const slugsData = await slugsUrl.json();
-    return slugsData;
-}
+const fetchSlugs = async () => {
+  const query = qs.stringify(
+    {
+      publicationState: "live",
+      fields: ["slug"],
+    },
+    {
+      encodeValuesOnly: true,
+    },
+  );
+
+  const url = `${apiUrl}/blogs?${query}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch slugs from CMS: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+
+  if (!data || !data.data) {
+    return [];
+  }
+
+  // Return the format expected by getStaticPaths
+  return data.data.map((post) => {
+    const { slug } = post.attributes;
+    return {
+      params: { slug, id: post.id },
+    };
+  });
+};
+
+export default fetchSlugs;
