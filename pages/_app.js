4168a3e1aef24857eb41fe7631d99219edca4a82
Julian Pena <julian@Julians-MacBook-Pro.local>
2026-04-23 11:08:50 -0500

    new pixewl code added

--- pages/_app.js
+++ pages/_app.js
@@ -147,6 +147,14 @@ function MyApp({ Component, pageProps }) {
         />
       </noscript>
 
+      <Script
+        id="stackadapt-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('ts','EFvi46X5uHLaQKX7qNrypQ');`,
+        }}
+      />
+
       <AppContext.Provider
         value={{
           theme,
