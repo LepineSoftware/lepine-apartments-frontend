fd61084c27d3ce6870f50fa73df9877b991208bd
Julian Pena <julian@Julians-MacBook-Pro.local>
2026-05-12 10:38:12 -0500

    new gtag added

--- pages/_app.js
+++ pages/_app.js
@@ -125,6 +125,17 @@ function MyApp({ Component, pageProps }) {
                 `,
           }}
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WMRTHDWF');
+                `,
+          }}
+        />
       </Head>
 
       <Script
@@ -188,6 +199,14 @@ function MyApp({ Component, pageProps }) {
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NMD6NT3K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WMRTHDWF"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
